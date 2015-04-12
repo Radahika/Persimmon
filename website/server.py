7e02dfbcab26d142bd3e8e2df5a6a87ef4dc10d7
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-11 23:27:09 -0700

    Request user's feed, then get back some home text

--- website/server.py
+++ website/server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 
+import json
 import os
 import pdb
 
@@ -20,9 +22,14 @@ def summary():
     summary = summarizer.summarize(text)
     return summary
 
-@app.route("/filter_page")
+@app.route("/filter_page", methods=["POST"])
 def filter_page():
-    return jsonify(result=5)
+    post_string = request.form.get("posts")
+
+    posts = json.loads(post_string)
+
+    return jsonify({"posts" : posts })
+
 
 if __name__ == "__main__":
     app.run()
