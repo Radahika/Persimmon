45c4c264ee534741b6ec8280896002a05dd5c615
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-11 19:45:07 -0700

    Try gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp"),
+  run = require("gulp-run"),
+  browserify = require("browserify"),
+  jshint = require("gulp-jshint"),
+  browserSync = require("browser-sync"),
+  source = require("vinyl-source-stream"),
+  buffer = require("vinyl-buffer"),
+  reactify = require("reactify"),
+  package = require("./package.json"),
+  reload = browserSync.reload;
+
+// Running Bower
+
+gulp.task("bower", function () {
+  run ("bower install").exec();
+})
+
+// Validation
+
+.task("lint", function () {
+  return gulp.src(package.paths.js)
+  .pipe(jshint())
+  .pipe(jshint.reporter("default"));
+})
+
+// JS compilation
+
+.task("js", function () {
+  return browserify(package.paths.app)
+  .transform(reactify)
+  .bundle()
+  .pipe(source(package.dest.app))
+  .pipe(gulp.dest(package.dest.dist));
+})
+
+.task("serve", ["bower", "lint", "js", "server"], function () {
+  return gulp.watch([
+    package.paths.js, package.paths.jsx, package.paths.html
+  ], [
+    "lint", "js"
+  ]);
+});
+
+
