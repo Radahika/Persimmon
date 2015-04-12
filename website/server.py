5ce00b32f3c6da5781c252c53f3e6a986c7b954d
Radahika <radhika.marvin@berkeley.edu>
2015-04-11 22:19:22 -0700

    downloaded different versions of boostrap.min.css nad bootstrap.css and displayed basic home page

--- website/server.py
+++ website/server.py
@@ -4,13 +4,18 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
+#pagination
+POSTS_PER_PAGE = 20
+
 @app.route("/")
 def login():
     return render_template("index.html")
 
 @app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/home/page/<int:page>")
+def home(page=1):
+    feed = [["Kevin Hwang", "Radhika is cool"], ["Radhika Marvin", "Kevin is cute"]]
+    return render_template("home.html", feed=feed)
 
 if __name__ == "__main__":
     app.run()
