c06929d9ecaee1eb66ac550f01f480a18e714314
Kevin Hwang <k3vinhwang@gmail.com>
2015-04-11 22:33:54 -0700

    Move index -> home

--- website/server.py
+++ website/server.py
@@ -10,18 +10,9 @@ from summarizer import summarizer
 app = Flask(__name__)
 app.debug = True
 
-#pagination
-POSTS_PER_PAGE = 20
-
 @app.route("/")
-def login():
-    return render_template("index.html")
-
-@app.route("/home")
-@app.route("/home/page/<int:page>")
-def home(page=1):
-    feed = [["Kevin Hwang", "Radhika is cool"], ["Radhika Marvin", "Kevin is cute"]]
-    return render_template("home.html", feed=feed)
+def home():
+    return render_template("home.html")
 
 @app.route("/summary", methods=["POST"])
 def summary():
